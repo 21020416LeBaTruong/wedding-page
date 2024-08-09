3ebd20d378cba08457b901322c941e1411c56073
21020416LeBaTruong <septruong0203@gmail.com>
2024-08-09 09:08:47 +0700

    wedding page

---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        "dark-purple": "#081A51",
+        "light-white": "rgba(255,255,255,0.17)",
+      },
+      animation: {
+        'loop-scroll': 'loop-scroll 25s linear infinite',
+      },
+      keyframes: {
+        'loop-scroll': {
+          from: { transform: 'translateX(0)' },
+          to: { transform: 'translateX(-100%)' },
+        }
+      },  
+      scrollBehavior: {
+        smooth: 'smooth',
+      }, 
+    },
+  },
+  plugins: [],
+}
