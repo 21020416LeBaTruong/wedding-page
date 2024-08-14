b32da82ec381344c5b9c5dc6ad9232529f36c177
21020416LeBaTruong <septruong0203@gmail.com>
2024-08-14 23:00:50 +0700

    fix

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -25,15 +25,33 @@ export default function Header() {
       window.removeEventListener('scroll', handleScroll);
     };
   }, []);
+  const handleResize = () => {
+    // If screen width is greater than or equal to 768px (md breakpoint), close the menu
+    if (window.innerWidth >= 768) {
+      setIsOpen(false);
+    }
+  };
 
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll);
+    window.addEventListener('resize', handleResize);
+    
+    // Initial check for screen size
+    handleResize();
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
   const headerHeight = 60; // Adjust this value according to your header height
 
   return (
     <div>
-      <div className={`fixed top-0 left-0 w-full transition-all duration-500 ${isScrolled || isOpen ? 'bg-white shadow-lg' : 'bg-transparent'} z-50`}>
+      <div className={`fixed top-0 left-0 w-full transition-all duration-500 ${isScrolled || isOpen ? 'bg-white' : 'bg-transparent'} z-50`}>
         <div className="flex justify-between items-center p-2 max-w-6xl mx-auto">
           <h1 className={`font-bold text-2xl ${isScrolled || isOpen ? 'text-black' : 'text-white'}`}>LOGO</h1>
-          <div className="hidden md:flex-1 md:flex md:justify-center">
+          <div className="hidden md:flex md:justify-center">
             <div className={`flex-col text-xl ${isScrolled || isOpen ? 'text-black' : 'text-white'} md:flex md:flex-row space-x-0 md:space-x-10`}>
               <Link to="home" smooth={true} duration={1000} offset={-headerHeight} className="cursor-pointer">
                 <button className="p-2 hover:underline font-serif font-bold">Home</button>
@@ -65,7 +83,7 @@ export default function Header() {
           </div>
         </div>
         {isOpen && (
-          <div className={`flex-col text-xl ${isScrolled || isOpen ? 'text-black' : 'text-white'} flex justify-center items-center -mt-11`}>
+          <div className={`md:hidden flex-col text-xl ${isScrolled || isOpen ? 'text-black' : 'text-white'} flex justify-center items-center -mt-11`}>
             <Link to="home" smooth={true} duration={1000} offset={-headerHeight} className="cursor-pointer">
               <button className="p-2 hover:underline font-serif font-bold">Home</button>
             </Link>
